cf45b7501564c152d82deecedde046906de50339
Aron Parker <hl3mukkel@gmail.com>
2017-03-18 03:23:56 +0100

    Initial commit

---
+++ KeyValues/KeyValueParseException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KeyValues
+{
+    public class KeyValueParseException : Exception
+    {
+        public KeyValueParseException(string message, int startIndex, int length) : base(message)
+        {
+            StartIndex = startIndex;
+            Length = length;
+        }
+        
+        public int StartIndex { get; }
+        public int Length { get; }
+    }
+}
